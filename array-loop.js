1c6c3795c96bb78201d2a4ee82298a26276f32cf
abirhasan33 <abir03512@gmail.com>
2022-01-28 14:48:06 +0600

    new text add

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+// var numbes = [40, 45, 53, 89, 20, 98, 82, 31, 20];
+// for(var i = 0; i < numbes.length; i++){
+//     var element = numbes[i];
+//     console.log(element);
+// }
+
+var items = ['botol', 'mobail', 'kolom', 'watch', 'pen', 'bock'];
+for(i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
--- for-loop.js
+++ for-loop.js
@@ -4,12 +4,30 @@
 //     roastGiven = roastGiven++;
 // }
 
-for (var roastGiven = 0; roastGiven < 7; roastGiven++){
-    console.log('rost den amar 2ta');
-    console.log(roastGiven);
-}
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('rost den amar 2ta');
+//     console.log(roastGiven);
+// }
 
-for (var number = 0; number < 10; number++){
-    console.log('valo basha deo');
-    console.log(number);
+// console.log('rinning a rie lock');
+// for (var number = 0; number < 10; number++){
+//     console.log('valo basha deo');
+//     console.log(number);
+// }
+
+// for ( var i = 0; i < 20; i++){
+//     // console.log('abir');
+//     console.log(i);
+// }
+
+// odd 
+// i+=2
+// i = i + 2 
+// for ( var i = 1; i <= 30; i = i + 2){
+//     console.log(i);
+// }
+
+// event number 
+for ( var i = 0; i <= 30; i = i + 2){
+    console.log(i);
 }
